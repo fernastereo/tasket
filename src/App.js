98ea2abee95487d74dcbd7a016a92ee29b3817cb
fernastereo <fernandoecueto@gmail.com>
2019-11-13 18:24:10 -0500

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Projects from "./pages/Projects";
+import Tasks from "./pages/Tasks";
 import "./App.css";
 
 export default function App() {
-  return <Projects />;
+  return (
+    <Switch>
+      <Route path="/" exact component={Projects} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/tasks" component={Tasks} />
+      <Redirect to="/" />
+    </Switch>
+  );
 }
---
+++ src/pages/Tasks.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Tasks extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Tasks</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
