dd66d3deb278e4eb053e97adc87b95084fdc862c
fernastereo <fernandoecueto@gmail.com>
2019-11-14 19:09:21 -0500

    Add lazy and Suspense for Pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./containers/NavBar";
-import Projects from "./pages/Projects";
-import Tasks from "./pages/Tasks";
+//import Projects from "./pages/Projects";
+//import Tasks from "./pages/Tasks";
 import SignIn from "./pages/SignIn";
 import "./App.css";
+
+const Projects = lazy(() => import("./pages/Projects"));
+const Tasks = lazy(() => import("./pages/Tasks"));
 
 export default function App() {
   return (
     <>
       <NavBar />
       <Switch>
-        <Route path="/" exact component={Projects} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/tasks" component={Tasks} />
+        <Route
+          path="/"
+          exact
+          render={() => {
+            return (
+              <Suspense fallback={<div>Loading...</div>}>
+                <Projects />
+              </Suspense>
+            );
+          }}
+        />
+        <Route
+          path="/projects"
+          render={() => {
+            return (
+              <Suspense fallback={<div>Loading...</div>}>
+                <Projects />
+              </Suspense>
+            );
+          }}
+        />
+        <Route
+          path="/tasks"
+          render={() => {
+            return (
+              <Suspense fallback={<div>Loading...</div>}>
+                <Tasks />
+              </Suspense>
+            );
+          }}
+        />
         <Route path="/signin" component={SignIn} />
         <Redirect to="/" />
       </Switch>
