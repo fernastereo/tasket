c39f4b1383cf5db516bbedad0742e5278aa74ff7
fernastereo <fernandoecueto@gmail.com>
2019-11-16 10:14:06 -0500

    Refactor UserProvider

--- src/App.js
+++ src/App.js
@@ -1,48 +1,29 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./containers/NavBar";
 //import Projects from "./pages/Projects";
 //import Tasks from "./pages/Tasks";
 import SignIn from "./pages/SignIn";
 import SignOut from "./pages/SignOut";
-import userContext from "./containers/UserContext";
+import { UserProvider } from "./containers/UserContext";
 import "./App.css";
 import ProtectedRoute from "./containers/ProtectedRoute";
 
 const Projects = lazy(() => import("./pages/Projects"));
 const Tasks = lazy(() => import("./pages/Tasks"));
 
-export default class App extends React.Component {
-  state = {
-    user: {
-      email: undefined,
-    },
-  };
-
-  setUser = user => {
-    this.setState({ user });
-  };
-
-  clearUser = () => {
-    this.setState({ user: { email: undefined } });
-  };
-
-  render() {
-    const { user } = this.state;
-    return (
-      <userContext.Provider
-        value={{ user, setUser: this.setUser, clearUser: this.clearUser }}
-      >
-        <NavBar />
-        <Switch>
-          <ProtectedRoute path="/" exact={true} component={Projects} />
-          <ProtectedRoute path="/projects" component={Projects} />
-          <ProtectedRoute path="/tasks" component={Tasks} />
-          <Route path="/signin" component={SignIn} />
-          <Route path="/signout" component={SignOut} />
-          <Redirect to="/" />
-        </Switch>
-      </userContext.Provider>
-    );
-  }
+export default function App() {
+  return (
+    <UserProvider>
+      <NavBar />
+      <Switch>
+        <ProtectedRoute path="/" exact={true} component={Projects} />
+        <ProtectedRoute path="/projects" component={Projects} />
+        <ProtectedRoute path="/tasks" component={Tasks} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signout" component={SignOut} />
+        <Redirect to="/" />
+      </Switch>
+    </UserProvider>
+  );
 }
--- src/containers/UserContext.js
+++ src/containers/UserContext.js
@@ -1,5 +1,32 @@
-import { createContext } from "react";
+import React, { Component, createContext } from "react";
 
 const userContext = createContext();
+
+export class UserProvider extends Component {
+  state = {
+    user: {
+      email: undefined,
+    },
+  };
+
+  setUser = user => {
+    this.setState({ user });
+  };
+
+  clearUser = () => {
+    this.setState({ user: { email: undefined } });
+  };
+
+  render() {
+    const { user } = this.state;
+    return (
+      <userContext.Provider
+        value={{ user, setUser: this.setUser, clearUser: this.clearUser }}
+      >
+        {this.props.children}
+      </userContext.Provider>
+    );
+  }
+}
 
 export default userContext;
